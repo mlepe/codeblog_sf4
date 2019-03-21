205ce6b89701cf1ff6d8f5b023e7db1e8d185313
Renaud Bredy <renaud.bredy@kibatic.com>
2019-03-21 08:05:29 +0100

    update cours

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
@@ -11,8 +11,24 @@ class HomepageController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function helloWorld()
+    public function homepage()
     {
-        return $this->render('base_home.html.twig');
+        $category = new Category();
+        $category->setName('Php');
+
+        $category2 = new Category();
+        $category2->setName('Java');
+
+        return $this->render('homepage/home.html.twig', [
+            'categories' => array($category, $category2),
+        ]);
+    }
+
+    /**
+     * @Route("/dummy-post", name="dummy_post")
+     */
+    public function dummyPost()
+    {
+        return $this->render('homepage/post.html.twig');
     }
 }
